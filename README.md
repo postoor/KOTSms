c083ca19448ed61acb3c9fa267be482c87b469d4
postoor <postoor@gmail.com>
2020-08-12 11:35:29 +0800

    Init Commit
    Can Send SMS By KOTSMS
    
    TODO: KOTSMS Callback

---
+++ README.md
@@ -0,0 +1,35 @@
+# KOTSms
+
+### Send SMS
+
+```php
+require __DIR__.'/vendor/autoload.php';
+
+use postoor\KOTSms\Sms;
+
+$sms = new Sms('username', 'password');
+
+$options = ['Ftcode' => 'Sms'.rand(0, 10000)];
+$messages = [];
+
+$kmsGID = $sms->send('<phone number>', '我是中文', $options, $messages);
+
+if ($kmsGID < 0) {
+    var_dump($messages);
+}
+
+echo $kmsGID;
+```
+
+#### Request Data
+
+| Key      | Desc.             | Max          | Note                   |
+| -------- | ----------------- | ------------ | ---------------------- |
+| username | Username          |              | required               |
+| password | Password          |              | required               |
+| dstaddr  | Phone Number      |              | required, phone_number |
+| smbody   | SMS Message       | 160          | required, message      |
+| dlvtime  | Delivery Time     |              | date(Y/m/d H:i:s)      |
+| vldtime  | Validity Period   | 1800 ~ 28800 |                        |
+| response | Callback Link     |              |                        |
+| Ftcode   | Verification Code | 36           |                        |
---
+++ src/Sms.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace postoor\KOTSms;
+
+use GUMP;
+use GuzzleHttp\Client;
+
+class Sms
+{
+    protected $username;
+
+    protected $password;
+
+    protected $url;
+
+    public function __construct(
+        $username,
+        $password,
+        $url = 'https://api2.kotsms.com.tw/kotsmsapi-1.php'
+    ) {
+        $this->username = $username;
+
+        $this->password = $password;
+
+        $this->url = $url;
+    }
+
+    /**
+     * Send SMS.
+     *
+     * @param string $target
+     * @param string $message
+     * @param array  $options
+     *
+     * @return int
+     */
+    public function send($target, $message, $options = [], array &$errorMessages = [])
+    {
+        $defaultParams = [
+            'dstaddr' => null,
+            'smbody' => null,
+            'dlvtime' => null,
+            'vldtime' => null,
+            'response' => null,
+            'Ftcode' => null,
+        ];
+
+        $params = array_merge($defaultParams, $options);
+        $params['dstaddr'] = $target;
+        $params['smbody'] = mb_convert_encoding($message, 'BIG5', 'auto');
+
+        $validate = $this->validate($params);
+        if ($validate !== true) {
+            $errorMessages = $validate;
+            throw new \Exception('Validate Failed');
+        }
+
+        $queryParams = [];
+        foreach ($params as $key => $value) {
+            if ($value == null) {
+                unset($params[$key]);
+                continue;
+            }
+            $queryParams[] = "{$key}={$value}";
+        }
+
+        $client = new Client();
+        $response = $client->request(
+            'GET',
+            $this->url."?username={$this->username}&password={$this->password}&".implode('&', $queryParams)
+        );
+
+        if ($response->getStatusCode() != 200) {
+            throw new \Exception('Connected Failed');
+        }
+        parse_str(trim($response->getBody()), $responseData);
+
+        if (!isset($responseData['kmsgid'])) {
+            throw new \Exception('No Response');
+        }
+
+        if ($responseData['kmsgid'] < 0) {
+            $errorMessages[] = $this->getMessageByCode($responseData['kmsgid']);
+        }
+
+        return (int) $responseData['kmsgid'];
+    }
+
+    /**
+     * Validate Data.
+     *
+     * @return GUMP
+     */
+    private function validate(array $data)
+    {
+        return GUMP::is_valid($data, [
+            'dstaddr' => 'required|phone_number',
+            'smbody' => 'required|max_len,160',
+            'dlvtime' => 'date,Y/m/d H:i:s',
+            'vldtime' => 'integer|min_numeric,1800|max_numeric,28800',
+            'response' => 'valid_url',
+            'Ftcode' => 'max_len,36',
+        ]);
+    }
+
+    /**
+     * Get Message By error code.
+     *
+     * @param string $code
+     *
+     * @return string
+     */
+    private function getMessageByCode($code)
+    {
+        $codeMsgMap = [
+            '-1' => 'CGI string error ，系統維護中或其他錯誤 ,帶入的參數異常,伺服器異常',
+            '-2' => '授權錯誤(帳號/密碼錯誤)',
+            '-4' => 'A Number違反規則 發送端 870短碼VCSN 設定異常',
+            '-5' => 'B Number違反規則 接收端 門號錯誤',
+            '-6' => 'Closed User 接收端的門號停話異常090 094 099 付費代號等',
+            '-20' => 'Schedule Time錯誤 預約時間錯誤 或時間已過',
+            '-21' => 'Valid Time錯誤 有效時間錯誤',
+            '-1000' => '發送內容違反NCC規範',
+            '-59999' => '帳務系統異常 簡訊無法扣款送出',
+            '-60002' => '您帳戶中的點數不足',
+            '-60014' => '該用戶已申請 拒收簡訊平台之簡訊 ( 2010 NCC新規)',
+            '-999949999' => '境外IP限制(只接受台灣IP發送，欲申請過濾請洽簡訊王客服)',
+            '-999959999' => '在12 小時內，相同容錯機制碼',
+            '-999969999' => '同秒, 同門號, 同內容簡訊',
+            '-999979999' => '鎖定來源IP',
+            '-999989999' => '簡訊為空',
+        ];
+
+        return $codeMsgMap[$code] ?? '未定義錯誤';
+    }
+
+    /**
+     * @TODO: Process KOT SMS response status
+     */
+    public function callback()
+    {
+        // code...
+    }
+}
